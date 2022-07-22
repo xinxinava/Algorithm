ba90513437ac0224fec63526b3fe2248f8afde21
xinxinava <1171492144@qq.com>
2022-07-22 23:01:42 +0800

    first commit

--- src/com/littlebean/interview/baidu20201/Baidu202019.java
+++ src/com/littlebean/interview/baidu20201/Baidu202019.java
@@ -1,5 +1,31 @@
 package com.littlebean.interview.baidu20201;
 
-public class Baidu202019 {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
+public class Baidu202019 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int m=scanner.nextInt();
+        List<Money> list=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            int value=scanner.nextInt();
+            int num=scanner.nextInt();
+            Money money=new Money(value, num);
+            list.add(money);
+        }
+        Collections.sort(list, (o1, o2) -> o2.value-o1.value);
+    }
+}
+
+class Money{
+    int value;
+    int num;
+    Money(int value, int num){
+        this.value=value;
+        this.num=num;
+    }
 }
