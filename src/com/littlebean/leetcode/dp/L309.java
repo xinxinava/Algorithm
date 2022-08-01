1763b5beb71f54e9f77f3aa34c96677f12e99dc8
xinxinava <1171492144@qq.com>
2022-08-01 15:14:10 +0800

    add

--- src/com/littlebean/leetcode/dp/L309.java
+++ src/com/littlebean/leetcode/dp/L309.java
@@ -2,7 +2,16 @@ package com.littlebean.leetcode.dp;
 
 public class L309 {
     public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int[][] dp=new int[n][4];
+        dp[0][0]=-prices[0];
+        for(int i=1;i<n;i++){
+            dp[i][0]=Math.max(dp[i-1][0], Math.max(dp[i-1][3],dp[i-1][1])-prices[i]);
+            dp[i][1]=Math.max(dp[i-1][1],dp[i-1][3]);
+            dp[i][2]=dp[i-1][0]+prices[i];
+            dp[i][3]=dp[i-1][2];
+        }
 
-        return 0;
+        return Math.max(dp[n-1][3],Math.max(dp[n-1][1],dp[n-1][2]));
     }
 }
