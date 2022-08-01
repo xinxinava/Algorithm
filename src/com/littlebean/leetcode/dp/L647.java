c106404a450885dafaad043635c597768a9aad43
xinxinava <1171492144@qq.com>
2022-08-01 17:39:03 +0800

    add

---
+++ src/com/littlebean/leetcode/dp/L647.java
@@ -0,0 +1,23 @@
+package com.littlebean.leetcode.dp;
+
+public class L647 {
+    public int countSubstrings(String s) {
+        int n=s.length();
+        boolean[][] dp=new boolean[n][n];
+        int res=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(s.charAt(i)==s.charAt(j)){
+                    if(j-i<=1){
+                        res++;
+                        dp[i][j]=true;
+                    }else if(dp[i+1][j-1]){
+                        res++;
+                        dp[i][j]=true;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
