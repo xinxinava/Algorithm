9823311f4f4ec8c0fc2e7767fafc868cd9efe3f7
xinxinava <1171492144@qq.com>
2022-09-01 10:20:20 +0800

    add

--- src/com/littlebean/other/HH.java
+++
@@ -1,7 +0,0 @@
-package com.littlebean.other;
-
-public interface HH {
-    int a=0;
-    int b=3;
-    int checked(int a, int b);
-}
--- src/com/littlebean/other/WW.java
+++
@@ -1,9 +0,0 @@
-package com.littlebean.other;
-
-public abstract class WW {
-    int a;
-    int b;
-    int c;
-    public int checked(int a, int b){return a;};
-
-}
