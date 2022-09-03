dea68b451641771dd200aa2d571abc3e8f2bc09b
xinxinava <1171492144@qq.com>
2022-09-03 23:32:40 +0800

    add

---
+++ src/com/littlebean/interview/actual/jd/JD3.java
@@ -0,0 +1,11 @@
+package com.littlebean.interview.actual.jd;
+
+import java.util.Scanner;
+
+public class JD3 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String s=scanner.nextLine();
+
+    }
+}
